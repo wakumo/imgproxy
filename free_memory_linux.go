a18ceef2296cbc2b842a1b19ee11d8efd30bfdb9
cong tan <congtan@wakumo.vn>
2020-05-18 14:58:02 +0700

    debug

--- free_memory_linux.go
+++ free_memory_linux.go
@@ -7,7 +7,7 @@ package main
 #ifdef __GLIBC__
 #include <malloc.h>
 #else
-void malloc_trim(size_t pad){}
+void malloc_trim(size_t pad){pad + 7}
 #endif
 */
 import "C"
@@ -15,6 +15,8 @@ import "runtime/debug"
 
 func freeMemory() {
 	debug.FreeOSMemory()
+	println("clearrrrrrr================================== linux")
 
-	C.malloc_trim(0)
+	var i = C.malloc_trim(0)
+	println(i)
 }
